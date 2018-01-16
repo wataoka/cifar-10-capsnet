899e09ec9421facc03636c47d4becb56aa3f72eb
Koki Wataoka <contact@wataoka.com>
2018-01-16 12:17:04 +0900

    fix mnist to cifar

--- capsulenet.py
+++ capsulenet.py
@@ -190,13 +190,13 @@ def manipulate_latent(model, data, args):
     print('-' * 30 + 'End: manipulate' + '-' * 30)
 
 
-def load_mnist():
+def load_cifar10():
     # 学習用データとテストデータを切り分け, シャッフルしたデータ
-    from keras.datasets import mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    from keras.datasets import cifar10
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-    x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.
-    x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    x_train = x_train.reshape(-1, 32, 32, 3).astype('float32') / 255.
+    x_test = x_test.reshape(-1, 32, 32, 3).astype('float32') / 255.
     y_train = to_categorical(y_train.astype('float32'))
     y_test = to_categorical(y_test.astype('float32'))
     return (x_train, y_train), (x_test, y_test)
@@ -238,7 +238,7 @@ if __name__ == "__main__":
         os.makedirs(args.save_dir)
 
     # データの読み込み
-    (x_train, y_train), (x_test, y_test) = load_mnist()
+    (x_train, y_train), (x_test, y_test) = load_cifar10()
 
     # モデル定義
     model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
